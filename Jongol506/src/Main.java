67f3bbabbb243e1c099de9c5274ed9ebf2f17490
Han SeungWoo <89821351+HanSeungWoo1@users.noreply.github.com>
2021-08-31 11:49:04 +0900

    1123

---
+++ Jongol506/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 170;
+		String b = "My height";
+		String c = "My weight";
+		double d = 68.6;
+		
+		System.out.println(b);
+		System.out.println(a);
+		System.out.println(c);
+		System.out.printf("%.6f",d);
+
+	}
+
+}
